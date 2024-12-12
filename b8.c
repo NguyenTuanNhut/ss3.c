07fc31fa9fc507f2f626ce082cd7e0f20b62e045
NguyenTuanNhut <nhutnguyen2k6.py@gmail.com>
2024-12-12 13:12:00 +0700

    Update b8.c

--- b8.c
+++ b8.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 int main (){
-    int number[4];
-    printf("nhap so \n");
-    for(int i=0;i<4;i++){
-        scanf("%d",&number[i]);
-    }
-    for(int i=3;i>-1;i--){
-        printf("%d ",number[i]);
-    }
-    return 0;
+    int number,number1,number2,number3,number4;
+    printf("nhap so nguyen to ");
+    scanf("%d",&number);
+    number1=number/1000;
+    number2=(number/100)%10;
+    number3=(number/10)%10;
+    number4=number%10;
+    printf("%d",number4);
+    printf("%d",number3);
+    printf("%d",number2);
+    printf("%d",number1);
+return 0;
+   
 }
