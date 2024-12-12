cabeac02a69a5a01acc3e38ba04b0b9de99ad50a
NguyenTuanNhut <nhutnguyen2k6.py@gmail.com>
2024-12-12 13:11:27 +0700

    Update b7.c

--- b7.c
+++ b7.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main(){
-    int n,s;
+    int n,s,number1,number2,number3,number4;
    
     printf ("nhap n");
     scanf ("%d",&n);
     s=0;
-    for(int i=0;i<4;i++){
-        s=s+(n%10);
-        n=n/10;
-    }
+    number1=n/1000;
+    number2=(n/100)%10;
+    number3=(n/10)%10;
+    number4=n%10;
+    s=number1+number2+number3+number4;
     printf("tong la :%d",s);
     return 0;
 }
